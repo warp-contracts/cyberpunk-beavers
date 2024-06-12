ada1240337c89897ab41f99a48c1626dece4270e
Asia <ziola.jm@gmail.com>
2024-06-12 14:24:24 +0200

    fix: process id

--- js/warp-ao-ids.js
+++ js/warp-ao-ids.js
@@ -1,5 +1,5 @@
 export default {
-  $17xPlIRro17U4ccXN9uWT8oFbLaEJfij3AQa12koWAvY',
+  processId_prod: '7xPlIRro17U4ccXN9uWT8oFbLaEJfij3AQa12koWAvY',
   moduleId_prod: 'AAmM9M_yr0Ik0VYTFB37wWNuxPBnusw2ngdJ723FpIk',
   processId_local: 'N8b2aPBXFhtZXbygq3wveXukUCTKKSFEu5qxB4CL-zU',
   moduleId_local: 'ZGU4Q6U_XBOVRxkTz7cxYKC7-iWdYg7TVHWihel7z9I',
