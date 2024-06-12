832c453740ac76b265f7cc22479e0320b7ba2fa3
Tadeuchi <tadeuszwl@gmail.com>
2024-06-12 15:25:13 +0200

    transfer tokens to after contract deploy

--- transfer-token.mjs
+++ transfer-token.mjs
@@ -1,8 +1,12 @@
 import { readFileSync } from 'node:fs';
 import { createDataItemSigner, message } from '@permaweb/aoconnect';
+import ids from './js/warp-ao-ids.js';
 
-// TODO: update tx id after game contract deployment and run it
-
+const envIdx = process.argv.indexOf('--env');
+if (envIdx < 0) {
+    throw new Error("Specify 'env' flash with either 'local' or 'prod' value");
+}
+const env = process.argv[envIdx + 1];
 const wallet = JSON.parse(readFileSync('.secrets/wallet.json').toString());
 
 await message({
@@ -14,7 +18,7 @@ await message({
   // Tags that the process will use as input.
   tags: [
     { name: 'From-Process', value: 'Iny8fK0S1FCSVVOIWubg2L9EXV1RFaxgRJwv5-mwEYk' }, // ;)
-    { name: 'Recipient', value: '7xPlIRro17U4ccXN9uWT8oFbLaEJfij3AQa12koWAvY' },
+    { name: 'Recipient', value: ids[`processId_${env}`] }, // game contract
     { name: 'Action', value: 'Transfer' },
     { name: 'Quantity', value: '1000000' },
   ],
