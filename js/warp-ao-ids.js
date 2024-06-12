66ebe6a2ee33c8dc5bf2cdbe23d60279286ef2b6
Tadeuchi <tadeuszwl@gmail.com>
2024-06-12 15:43:58 +0200

    new contract

--- js/warp-ao-ids.js
+++ js/warp-ao-ids.js
@@ -1,6 +1,6 @@
 export default {
-  processId_prod: '7xPlIRro17U4ccXN9uWT8oFbLaEJfij3AQa12koWAvY',
-  moduleId_prod: 'AAmM9M_yr0Ik0VYTFB37wWNuxPBnusw2ngdJ723FpIk',
+  processId_prod: 'vwDhdNmIhiXbcGh-kDPXnVE7WXc7N_0bAKNMk5qDybQ',
+  moduleId_prod: 'a9fhqAbl8dMS4TRfKgsUVUV5Kn94nfnVLEJJFpVJREs',
   processId_local: 'N8b2aPBXFhtZXbygq3wveXukUCTKKSFEu5qxB4CL-zU',
   moduleId_local: 'ZGU4Q6U_XBOVRxkTz7cxYKC7-iWdYg7TVHWihel7z9I',
   processId_dev: 'N8b2aPBXFhtZXbygq3wveXukUCTKKSFEu5qxB4CL-zU',
