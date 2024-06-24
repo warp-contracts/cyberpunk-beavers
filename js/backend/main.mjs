e20c7ff86224e7695bea7c0b0918966757c92741
Tadeuchi <tadeuszwl@gmail.com>
2024-06-24 14:54:38 +0200

    fix: very primitive fix for websocket fun with 2 processes

--- js/backend/main.mjs
+++ js/backend/main.mjs
@@ -1,5 +1,6 @@
 import { WebSocketServer, WebSocket } from 'ws';
 import {QuickJsPlugin} from "warp-contracts-plugin-quickjs";
+import ids from '../config/warp-ao-ids.js';
 import fs from "fs";
 
 const WS_PORT = 8097;
@@ -9,11 +10,23 @@ const wss = new WebSocketServer({ port: WS_PORT });
 
 const quickJSPlugin = new QuickJsPlugin({});
 
-const contractSource = fs.readFileSync('./dist/output.js', 'utf-8');
-const quickJs = await quickJSPlugin.process({
-  contractSource,
+/* ------ GAME ------*/
+const gameSource = fs.readFileSync('./dist/output-game.js', 'utf-8');
+const gameQuickJS = await quickJSPlugin.process({
+  contractSource: gameSource,
   binaryType: "release_sync"
 });
+const map = JSON.parse(fs.readFileSync('./assets/maps/map-2-30x30.json', 'utf-8'));
+let gameState = { rawMap: map };
+
+
+/* ------ CHAT ------*/
+const chatSource = fs.readFileSync('./dist/output-chat.js', 'utf-8');
+const chatQuickJs = await quickJSPlugin.process({
+  contractSource: chatSource,
+  binaryType: "release_sync"
+});
+let chatState = {};
 
 const processEnv = {
   Process: {
@@ -29,28 +42,46 @@ const processEnv = {
 }
 
 let txId = null;
-const map = JSON.parse(fs.readFileSync('./assets/maps/map-2-30x30.json', 'utf-8'));
-let state = { rawMap: map };
+
 
 // Event listener for WebSocket connections
-wss.on('connection', (ws) => {
-  console.log('A new client connected');
+wss.on('connection', (ws, request) => {
+  console.log('A new client connected', request.url);
+  ws.processId = request.url.substring(1);
 
   // Event listener for messages from the client
   ws.on('message', async (req) => {
-    // console.log('WS REQ: %s', message);
     const message = JSON.parse(req);
+    const actionTagValue = message.Tags.find((t) => t.name === 'Action').value;
+    const processTagValue = message.Tags.find((t) => t.name === 'From-Process').value;
+    console.log(`-------------------------------------------`)
+    console.log('WS REQ: %s', processTagValue, actionTagValue);
     txId = message.Id;
-    const result = await quickJs.handle(message, processEnv, state);
-    state = result.State;
-    if (result.Error) {
-      console.error(result.Error);
-    }
-    if (result.Output) {
-      logAndBroadcast(result.Output);
-    }
-    if (result.Messages.length) {
-      sendResponse(result.Messages[0]);
+
+    if (ids['chat_processId_dev'] === processTagValue) {
+      const result = await chatQuickJs.handle(message, processEnv, chatState);
+      chatState = result.State;
+      if (result.Error) {
+        console.error(result.Error);
+      }
+      if (result.Output) {
+        logAndBroadcast(result.Output, processTagValue);
+      }
+      if (result.Messages.length) {
+        chatState = await sendResponse(chatQuickJs, chatState, result.Messages[0]);
+      }
+    } else {
+      const result = await gameQuickJS.handle(message, processEnv, gameState);
+      gameState = result.State;
+      if (result.Error) {
+        console.error(result.Error);
+      }
+      if (result.Output) {
+        logAndBroadcast(result.Output, processTagValue);
+      }
+      if (result.Messages?.length) {
+        gameState = await sendResponse(gameQuickJS, gameState, result.Messages[0]);
+      }
     }
   });
 
@@ -60,11 +91,11 @@ wss.on('connection', (ws) => {
   });
 });
 
-function logAndBroadcast(message) {
+function logAndBroadcast(message, processId) {
   message.txId = txId;
   console.log('WS RES:', message.txId, message.cmd, message[message.cmd], message.scoreToDisplay);
   wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
+    if (client.readyState === WebSocket.OPEN && client.processId === processId) {
       setTimeout(() => {
         client.send(JSON.stringify(message));
       }, 150);
@@ -72,19 +103,22 @@ function logAndBroadcast(message) {
   });
 }
 
-function sendResponse(message) {
+async function sendResponse(quickJs, state, message) {
   console.log(message);
-  setTimeout(async () => {
-    const result = await quickJs.handle({
-      Tags: [
-        {name: 'Action', value: 'Debit-Notice'},
-        {name: 'Message', value: '123123'},
-        {name: 'Quantity', value: message.Quantity},
-        {name: 'Recipient', value: message.Recipient},
-      ]
-    }, processEnv, state);
-    state = result.State;
-  }, 600);
+  return await (new Promise((resolve, reject) => {
+    setTimeout(async () => {
+      const result = await quickJs.handle({
+        Tags: [
+          {name: 'Action', value: 'Debit-Notice'},
+          {name: 'Message', value: '123123'},
+          {name: 'Quantity', value: message.Quantity},
+          {name: 'Recipient', value: message.Recipient},
+        ]
+      }, processEnv, state);
+      resolve(result.State);
+    }, 600);
+  }));
+
 }
 
 console.log(`WebSocket server is running on port ${WS_PORT}`);
--- js/lib/websocket.js
+++ js/lib/websocket.js
@@ -1,7 +1,8 @@
 
-export async function initWebSocket() {
-  const ws = new WebSocket('ws://localhost:8097');
+export async function initWebSocket(moduleId, processId) {
+  const ws = new WebSocket(`ws://localhost:8097/${processId}`);
 
+  let nonce = 0;
   let currentListener = null;
   const unsubscribe = () => {
     if (currentListener) {
@@ -16,7 +17,7 @@ export async function initWebSocket() {
       ws.addEventListener('message', currentListener);
     },
     send: async (message) => {
-      const di = mockDataItem(message);
+      const di = mockDataItem(moduleId, processId, message, ++nonce);
       ws.send(JSON.stringify(di));
       return { id: di.Id };
     },
@@ -36,12 +37,13 @@ const messageListener = (target) => {
   }
 }
 
-function mockDataItem(data) {
-  const withTags = window.warpAO.data(data);
+function mockDataItem(moduleId, processId, data, nonce) {
+  const withTags = window.warpAO.data(moduleId, processId, data);
   return {
     Owner: localStorage.getItem('wallet_address'),
     Id: Math.random().toString(36).substring(2),
     Tags: withTags.tags,
     Timestamp: Date.now(),
+    Nonce: nonce
   };
 }
