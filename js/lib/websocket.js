a6d9a5643de1c2f7b17e496b08c96d00c1d5a9df
Tadeuchi <tadeuszwl@gmail.com>
2024-07-03 12:00:57 +0200

    fix: ws unlocking player

--- js/lib/websocket.js
+++ js/lib/websocket.js
@@ -28,6 +28,9 @@ const messageListener = (target) => {
   return (event) => {
     const response = JSON.parse(event.data);
     console.log(`Got message`, response.txId, response.cmd);
+    if (target.handleTx && response.lastTxs) {
+      target.handleTx(response.lastTxs);
+    }
     if (target.handleMessage) {
       target.handleMessage(response);
     }
