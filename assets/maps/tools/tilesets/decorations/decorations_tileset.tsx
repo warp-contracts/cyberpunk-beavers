22cc5aeaac847707569969cccd4d02131fe78c58
ppedziwiatr <ppedziwiatr@gmail.com>
2024-07-26 10:16:58 +0200

    maps template fix

--- assets/maps/tools/tilesets/decorations/decorations_tileset.tsx
+++ assets/maps/tools/tilesets/decorations/decorations_tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="decorations" tilewidth="48" tileheight="48" tilecount="5" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="decorations" tilewidth="48" tileheight="48" tilecount="14" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="48" height="48" source="tiles/A_newspaper_debris.png"/>
@@ -15,5 +15,32 @@
  </tile>
  <tile id="4">
   <image width="48" height="48" source="tiles/TT_sand_cover4.png"/>
+ </tile>
+ <tile id="5">
+  <image width="48" height="48" source="tiles/TT_sand_medium_rocks.png"/>
+ </tile>
+ <tile id="6">
+  <image width="48" height="48" source="tiles/TT_sand_pebbles.png"/>
+ </tile>
+ <tile id="7">
+  <image width="48" height="48" source="tiles/TT_sand_cover1-2.png"/>
+ </tile>
+ <tile id="8">
+  <image width="48" height="48" source="tiles/TT_sand_cover2-2.png"/>
+ </tile>
+ <tile id="9">
+  <image width="48" height="48" source="tiles/TT_sand_cover2-3.png"/>
+ </tile>
+ <tile id="10">
+  <image width="48" height="48" source="tiles/TT_sand_cover2-4.png"/>
+ </tile>
+ <tile id="11">
+  <image width="48" height="48" source="tiles/TT_sand_cover3-2.png"/>
+ </tile>
+ <tile id="12">
+  <image width="48" height="48" source="tiles/TT_sand_cover3-3.png"/>
+ </tile>
+ <tile id="13">
+  <image width="48" height="48" source="tiles/TT_sand_cover3-4.png"/>
  </tile>
 </tileset>
--- assets/maps/tools/tilesets/ground/ground_tileset.tsx
+++ assets/maps/tools/tilesets/ground/ground_tileset.tsx
@@ -1,85 +1,88 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="ground" tilewidth="48" tileheight="48" tilecount="27" columns="5" objectalignment="center">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="48" height="48" source="tiles/Dark_T_road_corner_bottom_left.png"/>
- </tile>
- <tile id="1">
-  <image width="48" height="48" source="tiles/Dark_T_road_corner_bottom_right.png"/>
- </tile>
- <tile id="2">
-  <image width="48" height="48" source="tiles/Dark_T_road_corner_top_left.png"/>
- </tile>
- <tile id="3">
-  <image width="48" height="48" source="tiles/Dark_T_road_corner_top_right.png"/>
- </tile>
- <tile id="4">
-  <image width="48" height="48" source="tiles/Dark_T_road_horizontal_bottom.png"/>
- </tile>
- <tile id="5">
-  <image width="48" height="48" source="tiles/Dark_T_road_horizontal_top.png"/>
- </tile>
- <tile id="6">
-  <image width="48" height="48" source="tiles/Dark_T_road_inside_corner_bottom_left.png"/>
- </tile>
- <tile id="7">
-  <image width="48" height="48" source="tiles/Dark_T_road_inside_corner_bottom_right.png"/>
- </tile>
- <tile id="8">
-  <image width="48" height="48" source="tiles/Dark_T_road_inside_corner_top_left.png"/>
- </tile>
- <tile id="9">
-  <image width="48" height="48" source="tiles/Dark_T_road_inside_corner_top_right.png"/>
- </tile>
- <tile id="10">
-  <image width="48" height="48" source="tiles/Dark_T_road_vertical_left_side.png"/>
- </tile>
- <tile id="11">
-  <image width="48" height="48" source="tiles/Dark_T_road_vertical_right_side.png"/>
- </tile>
- <tile id="12">
-  <image width="48" height="48" source="tiles/Dark_T_V1_sand.png"/>
- </tile>
- <tile id="13">
-  <image width="48" height="48" source="tiles/Dark_T_V2_sand.png"/>
- </tile>
- <tile id="14">
-  <image width="48" height="48" source="tiles/Light_T_road_corner_bottom_left.png"/>
- </tile>
- <tile id="15">
-  <image width="48" height="48" source="tiles/Light_T_road_corner_bottom_right.png"/>
- </tile>
- <tile id="16">
-  <image width="48" height="48" source="tiles/Light_T_road_corner_top_left.png"/>
- </tile>
- <tile id="17">
-  <image width="48" height="48" source="tiles/Light_T_road_corner_top_right.png"/>
- </tile>
- <tile id="18">
-  <image width="48" height="48" source="tiles/Light_T_road_horizontal_bottom.png"/>
- </tile>
- <tile id="19">
-  <image width="48" height="48" source="tiles/Light_T_road_horizontal_top.png"/>
- </tile>
- <tile id="20">
-  <image width="48" height="48" source="tiles/Light_T_road_inside_corner_bottom_left.png"/>
- </tile>
- <tile id="21">
-  <image width="48" height="48" source="tiles/Light_T_road_inside_corner_bottom_right.png"/>
- </tile>
- <tile id="22">
-  <image width="48" height="48" source="tiles/Light_T_road_inside_corner_top_left.png"/>
- </tile>
- <tile id="23">
-  <image width="48" height="48" source="tiles/Light_T_road_inside_corner_top_right.png"/>
- </tile>
- <tile id="24">
-  <image width="48" height="48" source="tiles/Light_T_road_vertical_left_side.png"/>
- </tile>
- <tile id="25">
-  <image width="48" height="48" source="tiles/Light_T_road_vertical_right_side.png"/>
- </tile>
- <tile id="26">
-  <image width="48" height="48" source="tiles/Light_T_sand.png"/>
- </tile>
-</tileset>
+<?xml version = "1.0" encoding = "UTF-8" ?>
+  <tileset version="1.10" tiledversion="1.10.2" name="ground" tilewidth="48" tileheight="48" tilecount="28" columns="5" objectalignment="center">
+    <grid orientation="orthogonal" width="1" height="1" />
+    <tile id="0">
+      <image width="48" height="48" source="tiles/Dark_T_road_corner_bottom_left.png" />
+    </tile>
+    <tile id="1">
+      <image width="48" height="48" source="tiles/Dark_T_road_corner_bottom_right.png" />
+    </tile>
+    <tile id="2">
+      <image width="48" height="48" source="tiles/Dark_T_road_corner_top_left.png" />
+    </tile>
+    <tile id="3">
+      <image width="48" height="48" source="tiles/Dark_T_road_corner_top_right.png" />
+    </tile>
+    <tile id="4">
+      <image width="48" height="48" source="tiles/Dark_T_road_horizontal_bottom.png" />
+    </tile>
+    <tile id="5">
+      <image width="48" height="48" source="tiles/Dark_T_road_horizontal_top.png" />
+    </tile>
+    <tile id="6">
+      <image width="48" height="48" source="tiles/Dark_T_road_inside_corner_bottom_left.png" />
+    </tile>
+    <tile id="7">
+      <image width="48" height="48" source="tiles/Dark_T_road_inside_corner_bottom_right.png" />
+    </tile>
+    <tile id="8">
+      <image width="48" height="48" source="tiles/Dark_T_road_inside_corner_top_left.png" />
+    </tile>
+    <tile id="9">
+      <image width="48" height="48" source="tiles/Dark_T_road_inside_corner_top_right.png" />
+    </tile>
+    <tile id="10">
+      <image width="48" height="48" source="tiles/Dark_T_road_vertical_left_side.png" />
+    </tile>
+    <tile id="11">
+      <image width="48" height="48" source="tiles/Dark_T_road_vertical_right_side.png" />
+    </tile>
+    <tile id="12">
+      <image width="48" height="48" source="tiles/Dark_T_V1_sand.png" />
+    </tile>
+    <tile id="13">
+      <image width="48" height="48" source="tiles/Dark_T_V2_sand.png" />
+    </tile>
+    <tile id="14">
+      <image width="48" height="48" source="tiles/Light_T_road_corner_bottom_left.png" />
+    </tile>
+    <tile id="15">
+      <image width="48" height="48" source="tiles/Light_T_road_corner_bottom_right.png" />
+    </tile>
+    <tile id="16">
+      <image width="48" height="48" source="tiles/Light_T_road_corner_top_left.png" />
+    </tile>
+    <tile id="17">
+      <image width="48" height="48" source="tiles/Light_T_road_corner_top_right.png" />
+    </tile>
+    <tile id="18">
+      <image width="48" height="48" source="tiles/Light_T_road_horizontal_bottom.png" />
+    </tile>
+    <tile id="19">
+      <image width="48" height="48" source="tiles/Light_T_road_horizontal_top.png" />
+    </tile>
+    <tile id="20">
+      <image width="48" height="48" source="tiles/Light_T_road_inside_corner_bottom_left.png" />
+    </tile>
+    <tile id="21">
+      <image width="48" height="48" source="tiles/Light_T_road_inside_corner_bottom_right.png" />
+    </tile>
+    <tile id="22">
+      <image width="48" height="48" source="tiles/Light_T_road_inside_corner_top_left.png" />
+    </tile>
+    <tile id="23">
+      <image width="48" height="48" source="tiles/Light_T_road_inside_corner_top_right.png" />
+    </tile>
+    <tile id="24">
+      <image width="48" height="48" source="tiles/Light_T_road_vertical_left_side.png" />
+    </tile>
+    <tile id="25">
+      <image width="48" height="48" source="tiles/Light_T_road_vertical_right_side.png" />
+    </tile>
+    <tile id="26">
+      <image width="48" height="48" source="tiles/Light_T_sand.png" />
+    </tile>
+    <tile id="27">
+      <image width="48" height="48" source="tiles/Light_T_sand_2.png" />
+    </tile>
+  </tileset>
