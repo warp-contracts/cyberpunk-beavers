0b8cf27816ac3814bc5c7c984cba4718829bc494
Asia <ziola.jm@gmail.com>
2024-08-22 11:07:49 +0200

    refactor: change wallet gui

--- src/components/ConnectWallet/utils.js
+++
@@ -1,5 +0,0 @@
-export async function generateSigner() {
-  const warpInst = WarpFactory.forMainnet();
-  const { jwk, address } = await warpInst.generateWallet();
-  return { signer: new ArweaveSigner(jwk), address };
-}
