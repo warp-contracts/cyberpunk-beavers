c6f28ab83a6d0b6d32a6933da8d598e025e4a288
Asia <ziola.jm@gmail.com>
2024-08-30 14:32:29 +0200

    round round tio

--- src/game/gui/MainScene/components/Stats.js
+++ src/game/gui/MainScene/components/Stats.js
@@ -90,7 +90,7 @@ export function Stats() {
             }),
             m(Label, {
               name: 'gained',
-              content: `${Math.round(info.gained * GameTreasure[name]?.baseVal) || '-'}`,
+              content: `${Math.round(info.gained * GameTreasure[name]?.baseVal * 100) / 100 || '-'}`,
               style: `mt-5 pl-15`,
               inlineStyle: `pr-10`,
             }),
