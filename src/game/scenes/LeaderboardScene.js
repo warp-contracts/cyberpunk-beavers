b472bd5b22e2fd85eb1e46676c3a5a5973bcf825
Tadeuchi <tadeuszwl@gmail.com>
2024-09-10 11:41:50 +0200

    setting up trunk token for next test session

--- src/game/scenes/LeaderboardScene.js
+++ src/game/scenes/LeaderboardScene.js
@@ -39,7 +39,7 @@ export default class LeaderboardScene extends Phaser.Scene {
       this.server.subscribe(this);
 
       var self = this;
-      const tokenType = GameTreasure.war.type;
+      const tokenType = GameTreasure.trunk.type;
       m.mount(showGui(), {
         view: function () {
           return m(LeaderboardGui, {
