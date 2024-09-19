dcb1c5296dcfd05f796fcf8f1c06b42bf273ddee
Asia <ziola.jm@gmail.com>
2024-09-19 14:03:03 +0200

    dynamic game tokens on leaderboard

--- src/game/scenes/LeaderboardScene.js
+++ src/game/scenes/LeaderboardScene.js
@@ -40,10 +40,9 @@ export default class LeaderboardScene extends Phaser.Scene {
       this.server.subscribe(this);
 
       var self = this;
-      const tokenTypes = ['tio', 'trunk'];
-      // const tokenTypes = Object.entries(self.gameTokens)
-      //   .filter(([, value]) => value.id != GAME_MODES[warpAO.config.mode].token && value.amount > 0)
-      //   .map(([type, value]) => type);
+      const tokenTypes = Object.entries(self.gameTokens)
+        .filter(([, value]) => value.id != GAME_MODES[warpAO.config.mode].token && value.amount > 0)
+        .map(([type, value]) => type);
 
       m.mount(showGui(), {
         view: function () {
