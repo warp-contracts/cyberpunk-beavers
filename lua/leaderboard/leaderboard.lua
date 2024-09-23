a1a6e1e0d386b82ed4a526aefcae338c17e7f6fb
Tadeuchi <tadeuszwl@gmail.com>
2024-09-23 13:43:04 +0200

    leaderboard updates, error msg and rsg, gun

--- lua/leaderboard/leaderboard.lua
+++ lua/leaderboard/leaderboard.lua
@@ -41,10 +41,12 @@ create table if not exists game_leaderboard(
     user_wallet_address TEXT NOT NULL,
     frags INTEGER NOT NULL DEFAULT 0,
     deaths INTEGER NOT NULL DEFAULT 0,
-    cb_coins INTEGER NOT NULL DEFAULT 0,
+    cbcoin INTEGER NOT NULL DEFAULT 0,
     tio INTEGER NOT NULL DEFAULT 0,
     war INTEGER NOT NULL DEFAULT 0,
     trunk INTEGER NOT NULL DEFAULT 0,
+    rsg INTEGER NOT NULL DEFAULT 0,
+    gun INTEGER NOT NULL DEFAULT 0,
     UNIQUE (game_id, user_wallet_address) ON CONFLICT REPLACE,
     FOREIGN KEY (game_id) REFERENCES game(id)
 );
@@ -54,116 +56,165 @@ create table if not exists global_leaderboard(
   user_wallet_address TEXT UNIQUE NOT NULL,
   frags INTEGER NOT NULL DEFAULT 0,
   deaths INTEGER NOT NULL DEFAULT 0,
-  cb_coins INTEGER NOT NULL DEFAULT 0,
+  cbcoin INTEGER NOT NULL DEFAULT 0,
   tio INTEGER NOT NULL DEFAULT 0,
   war INTEGER NOT NULL DEFAULT 0,
-  trunk INTEGER NOT NULL DEFAULT 0
+  trunk INTEGER NOT NULL DEFAULT 0,
+  rsg INTEGER NOT NULL DEFAULT 0,
+  gun INTEGER NOT NULL DEFAULT 0
 );
 ]])
 
+OrderBy = OrderBy or {
+    ["frags"] = true,
+    ["deaths"] = true,
+    ["cbcoin"] = true,
+    ["rsg"] = true,
+    ["tio"] = true,
+    ["war"] = true,
+    ["trunk"] = true,
+    ["gun"] = true
+}
 
 local function insertScores(hubProcessId, gameProcessId, playerScores)
-    print("print insertScores hub")
-    local hubSql = string.format("insert or ignore into game_hub_session(ao_process_id) VALUES ('%s')", hubProcessId)
-    db:exec(hubSql)
-
-    print("print insertScores game")
-    db:exec(string.format([[
-        with new_game(process_id) as (values ('%s'))
-        insert or ignore into game(ao_process_id, game_hub_session_id)
-        select process_id, game_hub_session.id
-        from new_game
-            join game_hub_session on game_hub_session.ao_process_id = '%s'
-    ]], gameProcessId, hubProcessId))
+    createHubAndGame(hubProcessId, gameProcessId)
 
     for wallet, data in pairs(playerScores) do
-        local gameScoresResult = insertSingleGameScores(gameProcessId, wallet, data)
-        print("insertSingleGameScores finished ")
-        if gameScoresResult ~= nil then
-            print("result " .. gameScoresResult)
-        end
-
-        local res = db:exec(string.format([[
-        insert into global_leaderboard(user_wallet_address, cb_coins, tio, war, trunk, frags, deaths)
-        values ('%s', %d, %d, %d, %d, %d, %d)
-        ON conflict
-            DO UPDATE
-            SET
-                cb_coins = cb_coins + excluded.cb_coins,
-                tio = tio + excluded.tio,
-                war = war + excluded.war,
-                war = trunk + excluded.trunk,
-                war = frags + excluded.frags,
-                war = deaths + excluded.deaths;
-        ]], wallet, data.tokens.cbcoin or 0, data.tokens.tio or 0, data.tokens.war or 0, data.tokens.trunk or 0,
-                data.kills.frags or 0, data.kills.deaths or 0))
-        print("Err: " .. db:errmsg())
-        print(string.format("wallet %s cb number %d", wallet, data.tokens.cbcoin))
-        print(res)
+        insertSingleGameScores(gameProcessId, wallet, data)
+        updateGlobalScore(wallet, data)
     end
 end
 
+function createHubAndGame(hubProcessId, gameProcessId)
+    local newHubStmt = db:prepare([[
+        insert or ignore into game_hub_session(ao_process_id) VALUES (?)
+    ]])
+    if not newHubStmt then
+        error(hubProcessId .. " preparing game hub session failure: " .. db:errcode() .. " " .. db:errmsg())
+    end
 
+    local newHubBind = newHubStmt:bind_values(hubProcessId)
+    if not newHubBind then
+        error(hubProcessId .. " binding game hub session failure: " .. db:errcode() .. " " .. db:errmsg())
+    end
+
+    executeStatement(newHubStmt, "New hub")
+
+    local newGameStmt = db:prepare([[
+        with new_game(process_id) as (values (?))
+        insert or ignore into game(ao_process_id, game_hub_session_id)
+        select process_id, game_hub_session.id
+        from new_game
+            join game_hub_session on game_hub_session.ao_process_id = ?
+    ]])
+    if not newGameStmt then
+        error(gameProcessId .. " preparing new game failure: " .. db:errcode() .. " " .. db:errmsg())
+    end
+
+    local newGameBind = newGameStmt:bind_values(gameProcessId, hubProcessId)
+    if not newGameBind then
+        error(gameProcessId .. " binding new game failure: " .. db:errcode() .. " " .. db:errmsg())
+    end
+
+    executeStatement(newGameStmt, "New game")
+end
 
 function insertSingleGameScores(game_process_id, wallet, data)
     local tokens = data.tokens
     local kills = data.kills
     local stmt = db:prepare([[
-        with scores(wallet, cb_coins, tio, war, trunk, frags, deaths) as (
-            values (?, ?, ?, ?, ?, ?, ?)
+        with scores(wallet, frags, deaths, cbcoin, tio, war, trunk, rsg, gun) as (
+            values (?, ?, ?, ?, ?, ?, ?, ?, ?)
         )
-        insert into game_leaderboard(user_wallet_address, game_id, cb_coins, tio, war, trunk, frags, deaths)
+        insert into game_leaderboard(user_wallet_address, game_id, frags, deaths, cbcoin, tio, war, trunk, rsg, gun)
         SELECT wallet, game.id,
-               coalesce(cb_coins, 0),
+               coalesce(frags, 0),
+               coalesce(deaths, 0),
+               coalesce(cbcoin, 0),
                coalesce(tio, 0),
                coalesce(war, 0),
                coalesce(trunk, 0),
-               coalesce(frags, 0),
-               coalesce(deaths, 0)
+               coalesce(rsg, 0),
+               coalesce(gun, 0)
         from scores
             cross join game on ao_process_id = ?;
     ]])
     if not stmt then
-        return "Error preparing statement: " .. (db:errmsg() or "Unknown error")
+        error("Error preparing statement: " .. (db:errmsg() or "Unknown error"))
     end
 
     local ok, bind_err = pcall(stmt.bind_values, stmt, wallet,
-        tokens.cbcoin or 0, tokens.tio or 0, tokens.war or 0, tokens.trunk or 0,
         kills.frags or 0, kills.deaths or 0,
+        tokens.cbcoin or 0, tokens.tio or 0, tokens.war or 0, tokens.trunk or 0,
+        tokens.rsg or 0, tokens.gun or 0,
         game_process_id)
     if not ok then
         stmt:finalize()
-        return "Error binding values: " .. (bind_err or "Unknown error")
+        error("Error binding values: " .. (bind_err or "Unknown error"))
     end
 
-    local step_result, step_err = stmt:step()
-    if step_result ~= sqlite3.DONE then
-        stmt:finalize()
-        return "Error executing statement: " .. (step_err or "Unknown error")
-    end
-
-    stmt:finalize()
-    return nil
+    executeStatement(stmt, "Single game scores")
 end
 
 
-
-local function readGlobalScore(limit, offset)
-    local scoreResult = {}
-    local stmt = db:prepare([[
-        SELECT * FROM global_leaderboard ORDER BY cb_coins DESC LIMIT ? OFFSET ?
+function updateGlobalScore(wallet, data)
+    local globalUpdateStmt = db:prepare([[
+        insert into global_leaderboard(user_wallet_address, frags, deaths, cbcoin, tio, war, trunk, rsg, gun)
+        values (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ON conflict
+            DO UPDATE
+            SET
+                frags = frags + excluded.frags,
+                deaths = deaths + excluded.deaths,
+                cbcoin = cbcoin + excluded.cbcoin,
+                tio = tio + excluded.tio,
+                war = war + excluded.war,
+                trunk = trunk + excluded.trunk,
+                rsg = rsg + excluded.rsg,
+                gun = gun + excluded.gun;
     ]])
-    stmt:bind_values(limit, offset)
+    if not globalUpdateStmt then
+        error("Preparing update global score failure: " .. db:errcode() .. " " .. db:errmsg())
+    end
+
+    local globalUpdateBind = globalUpdateStmt:bind_values(wallet,
+            data.kills.frags or 0, data.kills.deaths or 0,
+            data.tokens.cbcoin or 0, data.tokens.tio or 0, data.tokens.war or 0, data.tokens.trunk or 0,
+            data.tokens.rsg or 0, data.tokens.gun or 0)
+    if not globalUpdateBind then
+        error("Binding update global failure: " .. db:errcode() .. " " .. db:errmsg())
+    end
+
+    executeStatement(globalUpdateStmt, "Updating global score ")
+end
+
+
+local function readGlobalScore(orderBy, limit, offset)
+    local scoreResult = {}
+    local stmt = db:prepare(string.format([[
+        SELECT * FROM global_leaderboard ORDER BY %s DESC LIMIT ? OFFSET ?
+    ]], orderBy))
+    if not stmt then
+        error(db:errmsg())
+    end
+
+    local bind = stmt:bind_values(limit, offset)
+    if not bind then
+        error(db:errmsg())
+    end
+
 
     for row in stmt:nrows("") do
       table.insert(scoreResult, {
         wallet = row.user_wallet_address,
-        cbcoin = row.cb_coins,
+        frags = row.frags,
+        deaths = row.deaths,
+        cbcoin = row.cbcoin,
         tio = row.tio,
         war = row.war,
         trunk = row.trunk,
-        frags = row.frags,
-        deaths = row.deaths,
+        rsg = row.rsg,
+        gun = row.gun
       })
     end
     stmt:finalize()
@@ -171,16 +222,17 @@ local function readGlobalScore(limit, offset)
     return scoreResult
 end
 
-local function readGameScore(gameProcessId, limit, offset)
+local function readGameScore(gameProcessId, orderBy, limit, offset)
     local scoreResult = {}
-    local stmt = db:prepare([[
+    local stmt = db:prepare(string.format([[
         SELECT *
         FROM game_leaderboard
         JOIN game g on game_leaderboard.game_id = g.id
         WHERE g.ao_process_id = ?
-        ORDER BY cb_coins DESC LIMIT ? OFFSET ?
-    ]])
+        ORDER BY %s DESC LIMIT ? OFFSET ?
+    ]], orderBy))
     stmt:bind_values(gameProcessId, limit, offset)
+
 
     for row in stmt:nrows("") do
       table.insert(scoreResult, row)
@@ -188,6 +240,14 @@ local function readGameScore(gameProcessId, limit, offset)
     stmt:finalize()
 
     return scoreResult
+end
+
+function executeStatement(stmt, name)
+    local step_result, step_err = stmt:step()
+    if step_result ~= sqlite3.DONE then
+        stmt:finalize()
+        error(name .. " error executing statement: " .. (step_err or "Unknown error"))
+    end
 end
 
 
@@ -223,7 +283,11 @@ end)
 Handlers.add("globalScores", "GlobalScores", function (msg)
   local limit = msg.Tags.Limit or 10
   local offset = msg.Tags.Offset or 0
-  local scoresResult = readGlobalScore(limit, offset)
+  local orderBy = 'cbcoin'
+  if msg.Tags.OrderBy and OrderBy[msg.Tags.OrderBy] then
+    orderBy = msg.Tags.OrderBy
+  end
+  local scoresResult = readGlobalScore(orderBy, limit, offset)
   local resultEncoded = json.encode(scoresResult)
   print(resultEncoded)
   msg.reply({ Data = resultEncoded })
@@ -243,7 +307,11 @@ Handlers.add("gameScores", "GameScores", function (msg)
   local playerScores = json.decode(msg.Data)
   local limit = msg.Tags.Limit or 10
   local offset = msg.Tags.Offset or 0
-  local scoresResult = readGameScore(gameProcessId, limit, offset)
+  local orderBy = 'cbcoin'
+  if msg.Tags.OrderBy and OrderBy[msg.Tags.OrderBy] then
+    orderBy = msg.Tags.OrderBy
+  end
+  local scoresResult = readGameScore(gameProcessId, orderBy, limit, offset)
   local resultEncoded = json.encode(scoresResult)
   print(resultEncoded)
   msg.reply({ Data = resultEncoded })
