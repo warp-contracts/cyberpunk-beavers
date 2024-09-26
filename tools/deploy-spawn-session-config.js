48d8cf667435737bcac8d71d272de95c0684678c
Tadeuchi <tadeuszwl@gmail.com>
2024-09-26 14:18:52 +0200

    testing session, whitelist fix

--- tools/deploy-spawn-session-config.js
+++ tools/deploy-spawn-session-config.js
@@ -12,16 +12,16 @@ const hourSessionDelayMS = [
 ];
 const gameDurationMS = 5 * 60 * 1_000; // 5 minutes
 const walletsWhitelist = [
-  'Wu7uDPxsxTT4sm--rqJDIIJcfLrJQmX086GoPMXGVeY',
-  'Dj2ud7ui_rKtYwc_2cpGtNdlx6ifnX8w8VIksqIw42g',
-  '1bDUsm3L4EJOm3JZTItaXkvJAxxi8zrsRZ4KJN8HI_8',
-  'qCtnbl5rsbF-1qRhawIQArCbO4_2TUOtnFv5oEun9gM',
-  'wF_7BbSj8Yzlt7T44nMGXwJHmqn8t2Sv45LfxILNu_o',
-  '8ByvIowIXWOvEaOhpTeFUQQ_ArTiXAYzxwUSHr_R7P8', // ppe's generated wallet for the fucking Opera, fuck
-  '0x50Ff383E6b308069fD525B0ABa1474d9fe086743',
-  '0xC1AC0DFf4eb7c687Beaebe7151ea4F63E37fc8B5',
-  '0x27B5e6004511440e8b38405B8aF03aCe7B0Ae162',
-  '0x64937ab314bc1999396De341Aa66897C30008852',
+  // 'Wu7uDPxsxTT4sm--rqJDIIJcfLrJQmX086GoPMXGVeY',
+  // 'Dj2ud7ui_rKtYwc_2cpGtNdlx6ifnX8w8VIksqIw42g',
+  // '1bDUsm3L4EJOm3JZTItaXkvJAxxi8zrsRZ4KJN8HI_8',
+  // 'qCtnbl5rsbF-1qRhawIQArCbO4_2TUOtnFv5oEun9gM',
+  // 'wF_7BbSj8Yzlt7T44nMGXwJHmqn8t2Sv45LfxILNu_o',
+  // '8ByvIowIXWOvEaOhpTeFUQQ_ArTiXAYzxwUSHr_R7P8', // ppe's generated wallet for the fucking Opera, fuck
+  // '0x50Ff383E6b308069fD525B0ABa1474d9fe086743',
+  // '0xC1AC0DFf4eb7c687Beaebe7151ea4F63E37fc8B5',
+  // '0x27B5e6004511440e8b38405B8aF03aCe7B0Ae162',
+  // '0x64937ab314bc1999396De341Aa66897C30008852',
 ];
 
 export const TOKEN_CONTRACT_MOCK = {
