c14718a2a8efbfaf42ac9ad6d59bfb66bb5ec4e3
Tadeuchi <tadeuszwl@gmail.com>
2024-10-01 13:35:11 +0200

    prod open session 3-10-2024

--- src/game/config/warp-ao-ids.js
+++ src/game/config/warp-ao-ids.js
@@ -1,8 +1,8 @@
 export default {
-  hub_moduleId_prod: '7DIoC6Ctz09MAwtBs3NX25-BQZWtqnQqpBU09lKmb2I',
-  hub_processId_prod: '-7x_Zi_75dA9id7eCTiT2TisnuTFN9cyPJbscvu1yMw',
-  hub_moduleId_local: 'JJn8UoqFZozLs6AmOYpV5h2_PQRAckQqRh1zef48nWo',
-  hub_processId_local: 'O0p0tz3scPYyoaWVCtCFBYxvoCnHesiff3HrKA1-D48',
+  hub_moduleId_prod: 'OKlmHSY86geoGBc3a4nvCZEgmNRXsUwvvFDdXm-GyCo',
+  hub_processId_prod: 'G2qRpIcvA93MpUTh0_D9w72iCArmn4SjVu49NkVz0vI',
+  hub_moduleId_local: 'nhBdUAH9hBmF0T5CWnFQMAmj7p0gw7rPyKZJl38I0gY',
+  hub_processId_local: 'AmEd539oyU8QqtlV88PlItkYMORH60zCo9-iLsESOJg',
   hub_moduleId_dev: 'aaabbb',
   hub_processId_dev: 'aaabbb',
   token_processId_prod: 'rH_-7vT_IgfFWiDsrcTghIhb9aRclz7lXcK7RCOV2h8',
