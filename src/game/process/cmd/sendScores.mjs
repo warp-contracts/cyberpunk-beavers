a53992246f2328f605dd79fb0e71d661bcc5a873
ppedziwiatr <ppedziwiatr@gmail.com>
2024-10-04 14:18:32 +0200

    assign prizes for staying alive

--- src/game/process/cmd/sendScores.mjs
+++ src/game/process/cmd/sendScores.mjs
@@ -1,8 +1,22 @@
-import Const, { GAME_MODES } from '../../common/const.mjs';
+import Const, { GAME_MODES, GAMEPLAY_MODES, PRIZES } from '../../common/const.mjs';
 const { GameTreasure } = Const;
 
 export function sendScores(state) {
   if (!state.scoresSent) {
+    if (state.gameplayMode === GAMEPLAY_MODES.battleRoyale) {
+      const totalPrize = PRIZES[state.gameplayMode][state.mode];
+      console.log('Total prize for BR mode', totalPrize);
+      if (totalPrize > 0) {
+        const playersAlive = Object.values(state.players).filter((p) => p.stats.hp.current > 0);
+        if (playersAlive.length > 0) {
+          const singlePrize = Math.floor(totalPrize / playersAlive);
+          console.log('Single prize for BR mode', totalPrize);
+          for (const player in playersAlive) {
+            player.stats.coins.gained += singlePrize;
+          }
+        }
+      }
+    }
     const gameScores = {};
 
     for (let [wallet, data] of Object.entries(state.players)) {
