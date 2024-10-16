372ee51ea3376c2611fdfcfbe72deea9559c4b1b
ppedziwiatr <ppedziwiatr@gmail.com>
2024-10-16 10:16:57 +0200

    haunted map: b3m1

---
+++ public/assets/maps/v2/haunted/tilesets/haunted_a2_extended.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="haunted_a2_dark" tilewidth="48" tileheight="48" tilecount="1568" columns="56">
+ <grid orientation="orthogonal" width="0" height="0"/>
+ <image source="hauntedhouse_A2_dark_expanded.png" width="2688" height="1344"/>
+</tileset>
