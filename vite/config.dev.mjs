6ab3f0e76e49d534f7ad4e83c8490e570cb3477e
Asia <ziola.jm@gmail.com>
2024-10-16 08:25:51 +0200

    remove rsg mode from vite config

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -4,13 +4,12 @@ import Const from '../src/game/common/const.mjs';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  console.log(env.VITE_GAME_MODE);
   return {
     base: './',
     plugins: [],
     server: {
       port: 9001,
-      open: `index.html?env=dev${env.VITE_GAME_MODE == Const.GAME_MODES.rsg.type ? `&mode=${env.VITE_GAME_MODE}` : ''}`,
+      open: `index.html?env=dev`,
     },
   };
 });
--- vite/config.local.mjs
+++ vite/config.local.mjs
@@ -1,16 +1,13 @@
-import { defineConfig, loadEnv } from 'vite';
-import Const from '../src/game/common/const.mjs';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
   return {
     base: './',
     plugins: [],
     server: {
       port: 9001,
-      open: `index.html?env=local${env.VITE_GAME_MODE == Const.GAME_MODES.rsg.type ? `&mode=${env.VITE_GAME_MODE}` : ''}`,
+      open: `index.html?env=local`,
     },
   };
 });
