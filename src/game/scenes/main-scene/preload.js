653190d056441a41e4ef73cd6c15bbc4513736a2
Asia <ziola.jm@gmail.com>
2024-10-28 08:09:46 +0100

    haunted backgrounds compressed

--- src/game/scenes/main-scene/preload.js
+++ src/game/scenes/main-scene/preload.js
@@ -27,7 +27,7 @@ export function doPreloadAssets(mainScene) {
 
   //mainScene.load.image('cyberpunk_game_treasures', 'assets/images/game_treasures.png');
 
-  mainScene.load.image('post_apocalyptic_background', 'assets/images/background_graveyard.png');
+  mainScene.load.image('post_apocalyptic_background', 'assets/images/background_graveyard.jpg');
 
   mainScene.load.image('medal_gold', 'assets/images/medal-gold.png');
   mainScene.load.image('medal_silver', 'assets/images/medal-silver.png');
