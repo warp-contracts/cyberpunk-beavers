6be8870e5156cdf8e93bc2433195de04956b60a2
Asia <ziola.jm@gmail.com>
2024-12-20 15:11:39 +0100

    silent night

--- tools/create-map-tx.js
+++ tools/create-map-tx.js
@@ -1,7 +1,7 @@
 import { TurboFactory, defaultTurboConfiguration } from '@ardrive/turbo-sdk/node';
 import fs from 'fs';
 
-const mapName = 'b4m4.json';
+const mapName = 'b4m5.json';
 const file = `../public/assets/maps/v2/snow/${mapName}`;
 
 (async () => {
